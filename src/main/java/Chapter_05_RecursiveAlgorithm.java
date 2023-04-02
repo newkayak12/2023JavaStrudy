acaec6e3e2db72a2eb3fe591213426844717585b
newkayak12 <newkayak12@gmail.com>
2023-04-02 22:46:51 +0900

    java

--- src/main/java/Chapter_05_RecursiveAlgorithm.java
+++ src/main/java/Chapter_05_RecursiveAlgorithm.java
@@ -283,7 +283,6 @@ public class Chapter_05_RecursiveAlgorithm {
 
         System.out.println("NO RECURSIVE");
 
-
         move3(number, from, to);
 
     }
@@ -295,21 +294,54 @@ public class Chapter_05_RecursiveAlgorithm {
         if(num > 1) move2(num - 1, 6 - from - to, to);
     }
 
-    public void move3(int num, int from, int to) {
-        Stack< Map <String, Integer >> stack = new Stack<>();
+     void move3(int no, int x, int y) {
+//        int[] xstk = new int[100];
+//        int[] ystk = new int[100];
+//        int[] sstk = new int[100];		// 스택
+//        int ptr = 0; 						// 스택포인터
+//        int sw = 0;
 //
-//        while (num > 0) {
-//            stack.push(Map.of("num", num - 1, "from", from , "to", 6 - to -from));
-//            stack.push(Map.of("num", num, "from", from, "to", to));
-//            stack.push(Map.of("num", num - 1, "from", 6 - to -from , "to", to));
+//        while (true) {
+//            if (sw == 0 && no > 1) {
+//                xstk[ptr] = x;						// x의 값을 푸시
+//                ystk[ptr] = y;						// y의 값을 푸시
+//                sstk[ptr] = sw;						// sw의 값을 푸시
+//                ptr++;
+//                no = no - 1;
+//                y = 6  - x - y;
+//                continue;
+//            }
+//
+//            System.out.printf("원반[%d]을 %d 기둥에서 %d 기둥으로 이동\n", no, x, y);
+//
+//            if (sw == 1 && no > 1) {
+//                xstk[ptr] = x;						// x의 값을 푸시
+//                ystk[ptr] = y;						// y의 값을 푸시
+//                sstk[ptr] = sw;						// sw의 값을 푸시
+//                ptr++;
+//                no = no - 1;
+//                x = 6  - x - y;
+//                if (++sw == 2) sw = 0;
+//                continue;
+//            }
+//
+//
+//            do {
+//                if (ptr-- == 0) 					// 스택이 비어 있으면
+//                    return;
+//                x  = xstk[ptr]; 					// 값을 저장하고 있는 x를 팝
+//                y  = ystk[ptr]; 					// 값을 저장하고 있는 y을 팝
+//                sw = sstk[ptr] + 1;				// 값을 저장하고 있는 sw을 팝
+//                no++;
+//            } while (sw == 2);
 //        }
-
-        stack.push(Map.of("num", num - 1, "from", from , "to", 6 - to -from));
-        stack.push(Map.of("num", num, "from", from, "to", to));
-        stack.push(Map.of("num", num - 1, "from", 6 - to -from , "to", to));
-        System.out.println(stack);
-
     }
+
+    /**
+     * 8퀸 문제
+     *
+     */
+
 
 }
 
